90d39fccc614cb7ad7159c079c34140fdb59a9a1
heusserm <matt.heusser@gmail.com>
2018-12-19 07:49:29 -0500

    Junit 5 Eclipse Selenium Sample

---
+++ EclipseJavaSeleniumSample/EclipseSeleniumJunit5Example/src/SauceLabsHomePageTest.java
@@ -0,0 +1,26 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class SauceLabsHomePageTest {
+	@Test
+	public void site_header_is_on_home_page() {
+		
+		System.setProperty("webdriver.chrome.driver", "/opt/local/bin/chromedriver");
+		WebDriver browser = new ChromeDriver();
+		browser.get("https://www.saucelabs.com");
+	    WebElement href = browser.findElement(By.xpath("//a[@href='/beta/login']"));
+		assertTrue((href.isDisplayed()));
+
+		browser.close();       
+	}
+
+}
+
---
+++ EclipseJavaSeleniumSample/selenium-java/src/main/java/org/seleniumhq/selenium/selenium_java/App.java
@@ -0,0 +1,13 @@
+package org.seleniumhq.selenium.selenium_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
